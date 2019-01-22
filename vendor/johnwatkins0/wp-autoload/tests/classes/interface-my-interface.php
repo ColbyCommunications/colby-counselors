06feba039c3017838b1731980ea578d69e62b090
johnwatkins0 <johnwatkins0@gmail.com>
2019-01-21 21:59:01 -0600

    Remove extra files

--- vendor/johnwatkins0/wp-autoload/tests/classes/interface-my-interface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace MyNamespace;
-
-interface My_Interface {
-	public static function double_it( $it );
-}
--- vendor/johnwatkins0/wp-autoload/tests/classes/trait-my-trait.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace MyNamespace;
-
-trait My_Trait {
-    public static $my_trait_var = 'trait var';
-}

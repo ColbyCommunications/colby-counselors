657ec4f656343e037d8568f66b6b6397a3dca61b
Brandon K. Waltz <bwaltz6@gmail.com>
2021-09-20 13:53:39 -0400

    updates text

--- templates/template.php
+++ templates/template.php
@@ -11,7 +11,7 @@ if ( have_posts() ) : ?>
 <main class="colby-counselors-main main container mx-auto px-container mt-8 md:mt-8 lg:mt-16" id="main">
 
 	<h1 class="text-3xl font-bold" style="color: #273057"><?php colby_counselors_archive_title(); ?></h1>
-	<p class="mt-6">We can’t wait to learn more about you. For general inquiries, you can reach our office at (207) 859-4800 or <a href="mailto:admissions@colby.edu" class="underline" style="color: #002878;">admissions@colby.edu</a>. For Financial Aid inquiries, please reach out to (207) 859-4830 or <a href="mailto:finaid@colby.edu" class="underline" style="color: #002878;">finaid@colby.edu</a>. If you are looking to connect with a Colby admissions officer, use the above search bar to find your designated territory manager.</p>
+	<p class="mt-6">For general inquiries, please reach out to (207) 859-4800 or <a href="mailto:admissions@colby.edu" class="underline" style="color: #002878;">admissions@colby.edu</a>. For financial aid inquiries, please reach out to (207) 859-4830 or <a href="mailto:finaid@colby.edu" class="underline" style="color: #002878;">finaid@colby.edu</a>. To connect with your admissions counselor, use the search bar available on this page.</p>
 	<div id="colby-counselors-territory-picker"></div>
 
 	<?php if ( is_archive() ) : ?>

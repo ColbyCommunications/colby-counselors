8e5f6ccfdd5ab0983a0a0629f6fd2bcb508b98e1
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2022-06-30 14:42:08 -0400

    align territory filter to the left

--- templates/territory-picker.php
+++ templates/territory-picker.php
@@ -10,7 +10,7 @@ $international_territories = colby_counselors_get_global_territories();
 
 ?>
 
-<div id="location-filter" class="location-filter mt-8 pb-4 flex flex-col items-end" style="border-bottom: 1px solid black;"><div>
+<div id="location-filter" class="location-filter mt-8 pb-4 flex flex-col items-start" style="border-bottom: 1px solid black;"><div>
 	<div><h2 class="font-bold">Filter by Territory:</h2></div>
 	<div class="flex flex-col">
 

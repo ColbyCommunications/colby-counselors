fffc52b87e7a95f141d94085c0a82ca3955e8de5
Brandon K. Waltz <bwaltz6@gmail.com>
2024-05-06 10:38:26 -0400

    adds individual labels for filter fields

--- templates/territory-picker.php
+++ templates/territory-picker.php
@@ -11,13 +11,13 @@ $international_territories = colby_counselors_get_global_territories();
 ?>
 
 <div id="location-filter" class="location-filter mt-8 pb-4 flex flex-col items-start" style="border-bottom: 1px solid black;"><div>
-	<div><h2 class="font-bold">Filter by Territory:</h2></div>
+	<div><h2 class="font-bold mb-4">Filter by Territory:</h2></div>
 	<div class="flex flex-col">
 
 		<form id="location-filter" action="" method="GET">
-
+			<label for="location-pulldown" class="font-bold">U.S.</label>
 			<select name="territories" id="location-pulldown" onchange="this.form.submit()" class="mb-2 w-full tablet:w-28">
-				<option value="">-- U.S. --</option>
+				<option value="">-- None --</option>
 				<?php foreach ( $us_territories as $territory ) : ?>
 
 					<option value="<?php echo esc_attr( $territory->slug ); ?>"
@@ -33,8 +33,9 @@ $international_territories = colby_counselors_get_global_territories();
 			</select>
 		</form>
 		<form id="location-filter" action="" method="GET">
+			<label for="international" class="font-bold">International</label>
 			<select id="international" name="territories" onchange="this.form.submit()" class="w-full tablet:w-28">
-				<option value="">-- International --</option>
+				<option value="">-- None --</option>
 				<?php foreach ( $international_territories as $territory ) : ?>
 					<option value="<?php echo esc_attr( $territory->slug ); ?>"
 						<?php if ( get_query_var( 'territories' ) === $territory->slug ) : ?>

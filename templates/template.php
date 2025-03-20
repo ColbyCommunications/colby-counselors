f845efc692e0c5d435e5244684e465fcd7eacdcd
Brandon K. Waltz <bwaltz6@gmail.com>
2025-03-20 11:10:12 -0400

    Update template.php

--- templates/template.php
+++ templates/template.php
@@ -11,7 +11,8 @@ if ( have_posts() ) : ?>
 <main class="colby-counselors-main main container mx-auto px-container mt-8 md:mt-8 lg:mt-16" id="main">
 
 	<h1 class="text-3xl font-bold" style="color: #273057"><?php colby_counselors_archive_title(); ?></h1>
-	<p class="mt-6">For general inquiries, please reach out to (207) 859-4800 or <a href="mailto:admissions@colby.edu" class="underline" style="color: #002878;">admissions@colby.edu</a>. For financial aid inquiries, please reach out to (207) 859-4830 or <a href="mailto:finaid@colby.edu" class="underline" style="color: #002878;">finaid@colby.edu</a>. To connect with your admissions counselor, use the search bar available on this page.</p>
+	<p class="mt-6">For general inquiries, please reach out to (207) 859-4800 or <a href="mailto:admissions@colby.edu" class="underline hover:no-underline cursor-pointer" style="color: #002878;">admissions@colby.edu</a>. For financial aid inquiries, please reach out to (207) 859-4830 or <a href="mailto:finaid@colby.edu" class="underline hover:no-underline cursor-pointer" style="color: #002878;">finaid@colby.edu</a>. To connect with your admissions counselor, use the search bar available on this page.</p>
+	<p class="mt-2">To connect with a current student, please <a href="https://admissions.colby.edu/register/connect" class="underline hover:no-underline cursor-pointer" style="color: #002878;">click here</a>. </p>
 	<div id="colby-counselors-territory-picker"></div>
 
 	<?php if ( is_archive() ) : ?>
